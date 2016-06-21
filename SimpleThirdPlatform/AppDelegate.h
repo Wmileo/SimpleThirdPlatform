ba626d86d5b0ec11c14fc240f67990220d7253f0
chenruihua <chenruihua@mudi.com.cn>
2016-06-21 14:15:45 +0800

    Initial Commit

---
+++ SimpleThirdPlatform/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SimpleThirdPlatform
+//
+//  Created by ileo on 16/6/21.
+//  Copyright © 2016年 ileo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SimpleThirdPlatform/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  SimpleThirdPlatform
+//
+//  Created by ileo on 16/6/21.
+//  Copyright © 2016年 ileo. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
